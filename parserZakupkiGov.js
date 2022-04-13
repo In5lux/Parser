03c3fe395109e27d5e3011b04082765fc8603fab
In5lux <nikos_almeryda@mail.ru>
2022-04-13 22:10:19 +0300

    Url encoding class

--- parserZakupkiGov.js
+++ parserZakupkiGov.js
@@ -3,39 +3,24 @@ const cheerio = require('cheerio');
 
 const minPrice = 10_000_000;
 
+class UrlEncode {
+	constructor(query) {
+		this.query = query;
+		this.url = `https://zakupki.gov.ru/epz/order/extendedsearch/results.html?searchString=${encodeURIComponent(
+			this.query
+		)}&morphology=on&search-filter=%D0%94%D0%B0%D1%82%D0%B5+%D1%80%D0%B0%D0%B7%D0%BC%D0%B5%D1%89%D0%B5%D0%BD%D0%B8%D1%8F&pageNumber=1&sortDirection=false&recordsPerPage=_10&showLotsInfoHidden=false&sortBy=UPDATE_DATE&fz44=on&fz223=on&af=on&currencyIdGeneral=-1`;
+	}
+}
+
 const urls = [
-	[
-		'Организация командировок',
-		'https://zakupki.gov.ru/epz/order/extendedsearch/results.html?searchString=%D0%BE%D1%80%D0%B3%D0%B0%D0%BD%D0%B8%D0%B7%D0%B0%D1%86%D0%B8%D1%8F+%D0%BA%D0%BE%D0%BC%D0%B0%D0%BD%D0%B4%D0%B8%D1%80%D0%BE%D0%B2%D0%BE%D0%BA&morphology=on&search-filter=%D0%94%D0%B0%D1%82%D0%B5+%D1%80%D0%B0%D0%B7%D0%BC%D0%B5%D1%89%D0%B5%D0%BD%D0%B8%D1%8F&pageNumber=1&sortDirection=false&recordsPerPage=_10&showLotsInfoHidden=false&sortBy=UPDATE_DATE&fz44=on&fz223=on&af=on&currencyIdGeneral=-1',
-	],
-	[
-		'Организация деловых поездок',
-		'https://zakupki.gov.ru/epz/order/extendedsearch/results.html?searchString=%D0%BE%D1%80%D0%B3%D0%B0%D0%BD%D0%B8%D0%B7%D0%B0%D1%86%D0%B8%D1%8F+%D0%B4%D0%B5%D0%BB%D0%BE%D0%B2%D1%8B%D1%85+%D0%BF%D0%BE%D0%B5%D0%B7%D0%B4%D0%BE%D0%BA&morphology=on&pageNumber=1&sortDirection=false&recordsPerPage=_10&showLotsInfoHidden=false&sortBy=UPDATE_DATE&fz44=on&fz223=on&af=on&currencyIdGeneral=-1',
-	],
-	[
-		'Служебных поездок',
-		'https://zakupki.gov.ru/epz/order/extendedsearch/results.html?searchString=%D1%81%D0%BB%D1%83%D0%B6%D0%B5%D0%B1%D0%BD%D1%8B%D1%85+%D0%BF%D0%BE%D0%B5%D0%B7%D0%B4%D0%BE%D0%BA&morphology=on&search-filter=%D0%94%D0%B0%D1%82%D0%B5+%D1%80%D0%B0%D0%B7%D0%BC%D0%B5%D1%89%D0%B5%D0%BD%D0%B8%D1%8F&pageNumber=1&sortDirection=false&recordsPerPage=_10&showLotsInfoHidden=false&sortBy=UPDATE_DATE&fz44=on&fz223=on&af=on&currencyIdGeneral=-1',
-	],
-	[
-		'Выдворение',
-		'https://zakupki.gov.ru/epz/order/extendedsearch/results.html?searchString=%D0%B2%D1%8B%D0%B4%D0%B2%D0%BE%D1%80%D0%B5%D0%BD%D0%B8%D0%B5&morphology=on&search-filter=%D0%94%D0%B0%D1%82%D0%B5+%D1%80%D0%B0%D0%B7%D0%BC%D0%B5%D1%89%D0%B5%D0%BD%D0%B8%D1%8F&pageNumber=1&sortDirection=false&recordsPerPage=_10&showLotsInfoHidden=false&sortBy=UPDATE_DATE&fz44=on&fz223=on&af=on&currencyIdGeneral=-1',
-	],
-	[
-		'Бронирование билетов',
-		'https://zakupki.gov.ru/epz/order/extendedsearch/results.html?searchString=%D0%B1%D1%80%D0%BE%D0%BD%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5+%D0%B1%D0%B8%D0%BB%D0%B5%D1%82%D0%BE%D0%B2&morphology=on&search-filter=%D0%94%D0%B0%D1%82%D0%B5+%D1%80%D0%B0%D0%B7%D0%BC%D0%B5%D1%89%D0%B5%D0%BD%D0%B8%D1%8F&pageNumber=1&sortDirection=false&recordsPerPage=_10&showLotsInfoHidden=false&savedSearchSettingsIdHidden=&sortBy=UPDATE_DATE&fz44=on&fz223=on&af=on&placingWayList=&selectedLaws=&priceFromGeneral=&priceFromGWS=&priceFromUnitGWS=&priceToGeneral=&priceToGWS=&priceToUnitGWS=&currencyIdGeneral=-1&publishDateFrom=&publishDateTo=&applSubmissionCloseDateFrom=&applSubmissionCloseDateTo=&customerIdOrg=&customerFz94id=&customerTitle=&okpd2Ids=&okpd2IdsCodes=',
-	],
-	[
-		'Оформление авиабилетов',
-		'https://zakupki.gov.ru/epz/order/extendedsearch/results.html?searchString=%D0%BE%D1%84%D0%BE%D1%80%D0%BC%D0%BB%D0%B5%D0%BD%D0%B8%D1%8E+%D0%B0%D0%B2%D0%B8%D0%B0%D0%B1%D0%B8%D0%BB%D0%B5%D1%82%D0%BE%D0%B2&morphology=on&search-filter=%D0%94%D0%B0%D1%82%D0%B5+%D1%80%D0%B0%D0%B7%D0%BC%D0%B5%D1%89%D0%B5%D0%BD%D0%B8%D1%8F&pageNumber=1&sortDirection=false&recordsPerPage=_10&showLotsInfoHidden=false&sortBy=UPDATE_DATE&fz44=on&fz223=on&af=on&currencyIdGeneral=-1',
-	],
-	[
-		'Служебных командирований',
-		'https://zakupki.gov.ru/epz/order/extendedsearch/results.html?searchString=%D1%81%D0%BB%D1%83%D0%B6%D0%B5%D0%B1%D0%BD%D1%8B%D1%85+%D0%BA%D0%BE%D0%BC%D0%B0%D0%BD%D0%B4%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B9&morphology=on&pageNumber=1&sortDirection=false&recordsPerPage=_10&showLotsInfoHidden=false&sortBy=UPDATE_DATE&fz44=on&fz223=on&af=on&currencyIdGeneral=-1',
-	],
-	[
-		'Служебных командировок',
-		'https://zakupki.gov.ru/epz/order/extendedsearch/results.html?searchString=%D1%81%D0%BB%D1%83%D0%B6%D0%B5%D0%B1%D0%BD%D1%8B%D1%85+%D0%BA%D0%BE%D0%BC%D0%B0%D0%BD%D0%B4%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B9&morphology=on&pageNumber=1&sortDirection=false&recordsPerPage=_10&showLotsInfoHidden=false&sortBy=UPDATE_DATE&fz44=on&fz223=on&af=on&currencyIdGeneral=-1',
-	],
+	['Организация командировок', new UrlEncode('организация командировок').url],
+	['Организация деловых поездок', new UrlEncode('организация деловых поездок').url],
+	['Служебных поездок', new UrlEncode('cлужебных поездок').url],
+	['Выдворение', new UrlEncode('выдворение').url],
+	['Бронирование билетов', new UrlEncode('бронирование билетов').url],
+	['Оформление авиабилетов', new UrlEncode('оформление авиабилетов').url],
+	['Служебных командирований', new UrlEncode('служебных командирований').url],
+	['Служебных командировок', new UrlEncode('служебных командировок').url],
 ];
 
 let parseResults = [];
@@ -46,9 +31,14 @@ const parseData = (html, minPrice) => {
 
 	$('.search-registry-entry-block').each((i, elem) => {
 		const result = {
-			N: $(elem)
+			number: $(elem)
 				.find('.registry-entry__header-mid__number a')
 				.text()
+				.trim(),
+			type: $(elem)
+				.find('.registry-entry__header-top__title')
+				.text()
+				.replace(/\s{2,}/g, ' ')
 				.trim(),
 			customer: $(elem)
 				.find('.registry-entry__body-href a')
@@ -72,6 +62,7 @@ const parseData = (html, minPrice) => {
 					.find('.registry-entry__header-mid__number a')
 					.attr('href'),
 		};
+		result.documents = result.link.replace('common-info', 'documents');
 		if (
 			!parseResults.filter(
 				(parseResult) => parseResult.link == result.link
@@ -130,8 +121,3 @@ const getData = (url) => {
 };
 
 urls.forEach((url) => getData(url));
-
-// setTimeout(() => {
-// 	console.log('--------------------------------------------');
-// 	console.log(parseResults);
-// }, 5000);
